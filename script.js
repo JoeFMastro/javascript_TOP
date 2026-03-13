af296e1bea44e367d375022ee40b7ab8f21c45d3
Joel <joelmastroiaco@gmail.com>
2026-03-13 16:39:58 -0300

    getComputerChoice completed

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+  let number = Math.floor(Math.random() * 3);
+  if (number == 0) {
+    return "rock";
+  }
+  else if (number == 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+//console.log(getComputerChoice());
